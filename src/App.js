337943c72ccc7687a6a4825c1905a40f76bf7dbc
Emir Savranogullari <emir@emirsavran.com>
2020-04-18 21:08:41 +0300

    add react-router-dom to dependencies, configure routes simply

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Layout from './components/Layout';
 
 function App() {
   return (
-    <Layout>
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </Layout>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <p>Home</p>
+          </Route>
+          <Route path="/new">
+            <p>Add New Link</p>
+          </Route>
+          <Route path="*">
+            <p>Not Found</p>
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
