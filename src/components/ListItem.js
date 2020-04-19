d5bd0b510e321c3ab76305a8916a46ddc3cf77a8
Emir Savranogullari <emir@emirsavran.com>
2020-04-19 18:25:17 +0300

    take ListItem mouse over behaviour from js and give it to css

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Button, Card, CardActions, CardContent, Typography, makeStyles, Box, Paper, Fade, IconButton,
+  Button, Card, CardActions, CardContent, Typography, makeStyles, Box, Paper, IconButton,
 } from '@material-ui/core';
 import { ArrowUpward, ArrowDownward, RemoveCircleOutline } from '@material-ui/icons';
 
@@ -13,7 +13,11 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'row',
     '&:hover': {
       backgroundColor: '#ffffff',
-      transition: 'background-color 195ms linear',
+      transition: 'background-color 200ms linear',
+    },
+    '&:hover $removeButton': {
+      opacity: 1,
+      transition: 'opacity 200ms linear',
     },
     [theme.breakpoints.only('xs')]: {
       flexDirection: 'column',
@@ -37,6 +41,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   removeButton: {
+    opacity: 0,
     position: 'absolute',
     top: 0,
     right: 0,
@@ -45,46 +50,35 @@ const useStyles = makeStyles((theme) => ({
 
 function ListItem({ name, url }) {
   const classes = useStyles();
-  const [isMouseOver, setIsMouseOver] = useState(false);
 
   return (
-    <>
-      <Card
-        className={classes.card}
-        elevation={0}
-        variant="outlined"
-        onMouseEnter={() => setIsMouseOver(true)}
-        onMouseLeave={() => setIsMouseOver(false)}
-      >
-        <Fade in={isMouseOver}>
-          <IconButton className={classes.removeButton} aria-label="remove link">
-            <RemoveCircleOutline color="error" />
-          </IconButton>
-        </Fade>
-        <Paper elevation={0} variant="outlined" className={classes.pointBox}>
-          <Typography variant="h3">6</Typography>
-          <Typography>Points</Typography>
-        </Paper>
-        <Box>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="h3">
-              {`(${url})`}
-            </Typography>
-          </CardContent>
-          <CardActions className={classes.actions}>
-            <Button color="primary" startIcon={<ArrowUpward />}>
-              Up Vote
-            </Button>
-            <Button color="primary" startIcon={<ArrowDownward />}>
-              Down Vote
-            </Button>
-          </CardActions>
-        </Box>
-      </Card>
-    </>
+    <Card className={classes.card} elevation={0} variant="outlined">
+      <IconButton className={classes.removeButton} aria-label="remove link">
+        <RemoveCircleOutline color="error" />
+      </IconButton>
+      <Paper elevation={0} variant="outlined" className={classes.pointBox}>
+        <Typography variant="h3">6</Typography>
+        <Typography>Points</Typography>
+      </Paper>
+      <Box>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h3">
+            {`(${url})`}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions}>
+          <Button color="primary" startIcon={<ArrowUpward />}>
+            Up Vote
+          </Button>
+          <Button color="primary" startIcon={<ArrowDownward />}>
+            Down Vote
+          </Button>
+        </CardActions>
+      </Box>
+    </Card>
   );
 }
 
