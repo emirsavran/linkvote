955c31106bb59f6195922f15080994af1f17a706
Emir Savranogullari <emir@emirsavran.com>
2020-04-20 17:53:41 +0300

    create Layout.test

---
+++ src/__tests__/Layout.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Layout from '../components/Layout';
+
+it('should render correctly', () => {
+  const { getByText } = render(
+    <Layout>
+      <p>Children</p>
+    </Layout>,
+  );
+
+  const appBarText = getByText('LinkVOTE Challenge');
+  expect(appBarText).toBeInTheDocument();
+
+  const children = getByText('Children');
+  expect(children).toBeInTheDocument();
+});
